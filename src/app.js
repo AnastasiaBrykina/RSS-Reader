77b1663be1f0f4710a60685e821049d461fef39d
AnastasiaBrykina <anastasiaaa.6934.@gmail.com>
2023-12-15 13:16:06 +0400

    update index.html src/app.js src/view.js

--- src/app.js
+++ src/app.js
@@ -1,53 +1,160 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable import/no-extraneous-dependencies */
 import * as yup from 'yup';
 import _ from 'lodash';
+import i18next from 'i18next';
+import axios from 'axios';
 import watch from './view';
+import resources from './locales/index.js';
 
-const validate = (fields, feeds) => {
+const routes = {
+  path: () => 'https://allorigins.hexlet.app/get?disableCache=true&url=',
+};
+
+yup.setLocale({
+  mixed: {
+    notOneOf: () => 'errors.validation.exists',
+  },
+  string: {
+    url: () => 'errors.validation.invalid',
+  },
+});
+
+const validate = (fields, urls) => {
   const schema = yup.object({
-    url: yup.string().url().notOneOf(feeds),
+    url: yup.string().url().notOneOf(urls),
   });
   return schema.validate(fields, { abortEarly: false });
 };
 
+const rssParserFeed = (content) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(content, 'application/xml');
+  return doc;
+};
+
+const initialFeeds = (content, watchedState) => {
+  const domTree = rssParserFeed(content);
+  if (domTree.querySelector('parsererror')) {
+    throw new Error('parsererror');
+  }
+
+  watchedState.urls.push(watchedState.form.fields.url);
+  watchedState.errKey = '';
+
+  const channel = domTree.querySelector('channel');
+  const titleFedd = channel.querySelector('title');
+  const descriptionFeed = channel.querySelector('description');
+  const linkFeed = channel.querySelector('link');
+  const posts = channel.querySelectorAll('item');
+
+  const uniqueIdFedd = _.uniqueId();
+  const initialFeed = {
+    id: uniqueIdFedd,
+    title: titleFedd.textContent,
+    description: descriptionFeed.textContent,
+    link: linkFeed.textContent,
+  };
+
+  const mappedPosts = Array.from(posts).map((post) => {
+    const titlePost = post.querySelector('title');
+    const linkPost = post.querySelector('link');
+    const descriptionPost = post.querySelector('description');
+
+    const initialPost = {
+      id: _.uniqueId(),
+      feedId: uniqueIdFedd,
+      title: titlePost.textContent,
+      description: descriptionPost.textContent,
+      link: linkPost.textContent,
+    };
+
+    return initialPost;
+  });
+
+  watchedState.posts = [...mappedPosts, ...watchedState.posts];
+  watchedState.feeds.push(initialFeed);
+  watchedState.form.processState = 'finished';
+};
+
+const errorsController = (err, watchedState) => {
+  const { currentCheck } = watchedState;
+  switch (currentCheck) {
+    case 'validation':
+      watchedState.errKey = err.inner[0].message;
+      break;
+    default:
+      watchedState.errKey = watchedState.mappingErrors[currentCheck];
+      break;
+  }
+};
+
+const submitController = (event, watchedState) => {
+  event.preventDefault();
+  const dataForm = new FormData(event.target);
+  const inputValue = dataForm.get('url').trim();
+  watchedState.form.fields.url = inputValue;
+  watchedState.currentCheck = 'validation';
+  watchedState.form.processState = 'sending';
+  validate(watchedState.form.fields, watchedState.urls)
+    .then(() => {
+      watchedState.currentCheck = 'network';
+      return axios.get(`${routes.path()}${encodeURIComponent(inputValue)}`);
+    })
+    .then((res) => {
+      watchedState.currentCheck = 'parseRss';
+      const content = res.data.contents;
+      initialFeeds(content, watchedState);
+    })
+    .catch((err) => {
+      watchedState.form.processState = 'failed';
+      errorsController(err, watchedState);
+    });
+};
+
 const app = () => {
+  const elements = {
+    form: document.querySelector('.rss-form'),
+    btn: document.querySelector('#rss-btn'),
+    urlInput: document.querySelector('#url-input'),
+    feedback: document.querySelector('.feedback'),
+    postsContainer: document.querySelector('.posts'),
+    feedsContainer: document.querySelector('.feeds'),
+  };
+
+  const defaultLang = 'ru';
+
   const initialState = {
     form: {
       fields: {
         url: '',
       },
-      isValid: true,
+      processState: 'filling',
     },
+    urls: [],
     feeds: [],
-    errors: null,
+    posts: [],
+    currentCheck: null,
+    mappingErrors: {
+      network: 'errors.network.networkErr',
+      parseRss: 'errors.parser.invalid',
+    },
+    errKey: null,
   };
 
-  const elements = {
-    form: document.querySelector('.rss-form'),
-    btn: document.querySelectorAll('#rss-btn'),
-    urlInput: document.querySelector('#url-input'),
-  };
+  const i18n = i18next.createInstance();
+  const initi18n = i18n
+    .init({
+      lng: defaultLang,
+      debug: true,
+      resources,
+    })
+    .then((t) => t);
 
-  const watchedState = watch(elements, initialState);
+  const watchedState = watch(elements, initialState, initi18n);
 
   elements.form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const dataForm = new FormData(event.target);
-    const inputValue = dataForm.get('url').trim();
-    watchedState.form.fields.url = inputValue;
-    validate(watchedState.form.fields, watchedState.feeds)
-      .then(() => {
-        watchedState.errors = {};
-        watchedState.feeds.push(inputValue);
-        watchedState.form.isValid = true;
-      })
-      .catch((err) => {
-        const validateErr = _.keyBy(err.inner, 'path');
-        watchedState.errors = validateErr;
-        watchedState.form.isValid = false;
-      });
-
-    console.log(initialState);
+    submitController(event, watchedState);
   });
 };
 
--- src/view.js
+++ src/view.js
@@ -2,25 +2,167 @@
 /* eslint-disable no-unused-vars */
 import onChange from 'on-change';
 
-const renderErrors = (elements, state) => {
-  const { errors } = state;
-  const { form, urlInput } = elements;
+const makePostsList = (posts, i18n) => {
+  const ul = document.createElement('ul');
+  ul.classList.add('list-group', 'border-0', 'rounded-0');
+  posts.forEach((post) => {
+    const {
+      id, title, description, link,
+    } = post;
+    const li = document.createElement('li');
+    li.classList.add(
+      'list-group-item',
+      'd-flex',
+      'justify-content-between',
+      'align-items-start',
+      'border-0',
+      'border-end-0',
+    );
+    ul.append(li);
 
-  if (Object.keys(errors).length === 0) {
+    const a = document.createElement('a');
+    a.classList.add('fw-bold');
+    a.setAttribute('href', link);
+    a.setAttribute('data-id', id);
+    a.setAttribute('target', '_blank');
+    a.setAttribute('rel', 'noopener noreferrer');
+    a.textContent = title;
+    li.append(a);
+
+    const button = document.createElement('button');
+    button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+    button.setAttribute('type', 'button');
+    button.setAttribute('data-id', id);
+    // button.setAttribute('data-bs-toggle', 'modal');
+    // button.setAttribute('data-bs-target', '#modal');
+    i18n.then((t) => {
+      button.textContent = t('posts.button');
+    });
+    li.append(button);
+  });
+
+  return ul;
+};
+
+const makeFeedsList = (feeds) => {
+  const ul = document.createElement('ul');
+  ul.classList.add('list-group', 'border-0', 'rounded-0');
+  feeds.forEach((feed) => {
+    const { title, description } = feed;
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'border-0', 'border-end-0');
+    ul.prepend(li);
+
+    const h3 = document.createElement('h3');
+    h3.classList.add('h6', 'm-0');
+    h3.textContent = title;
+
+    const p = document.createElement('p');
+    p.classList.add('m-0', 'small', 'text-black-50');
+    p.textContent = description;
+
+    li.append(h3, p);
+  });
+
+  return ul;
+};
+
+const makeCardBody = (i18n, type) => {
+  const card = document.createElement('div');
+  card.classList.add('card', 'border-0');
+
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+  card.append(cardBody);
+
+  const cardTitle = document.createElement('h2');
+  cardTitle.classList.add('card-title', 'h4');
+  i18n.then((t) => {
+    cardTitle.textContent = t(`${type}.title`);
+  });
+  cardBody.append(cardTitle);
+
+  return card;
+};
+
+const renderFeeds = (elements, state, i18n) => {
+  const { feedsContainer, postsContainer } = elements;
+  const { feeds, posts } = state;
+
+  feedsContainer.innerHTML = '';
+  postsContainer.innerHTML = '';
+
+  const divFeeds = makeCardBody(i18n, 'feeds');
+  feedsContainer.append(divFeeds);
+
+  const divPosts = makeCardBody(i18n, 'posts');
+  postsContainer.append(divPosts);
+
+  const feedsList = makeFeedsList(feeds);
+  divFeeds.append(feedsList);
+
+  const postsList = makePostsList(posts, i18n);
+  divPosts.append(postsList);
+};
+
+const renderErrors = (elements, state, i18n) => {
+  const { errKey } = state;
+  const { urlInput, feedback } = elements;
+
+  if (!errKey) {
     urlInput.classList.remove('is-invalid');
-    form.reset();
-    urlInput.focus();
+    i18n.then((t) => {
+      feedback.classList.remove('text-danger');
+      feedback.classList.add('text-success');
+      feedback.textContent = t('errors.validation.valid');
+    });
   } else {
     urlInput.classList.add('is-invalid');
+    i18n.then((t) => {
+      feedback.classList.remove('text-success');
+      feedback.classList.add('text-danger');
+      feedback.textContent = t(errKey);
+    });
   }
 };
 
-const watch = (elements, state) => {
-  const watchedState = onChange(state, (path) => {
+const handleProcessState = (elements, processState) => {
+  const { form, btn, urlInput } = elements;
+  switch (processState) {
+    case 'sending':
+      btn.setAttribute('disabled', '');
+      urlInput.setAttribute('disabled', '');
+      break;
+    case 'failed':
+      btn.removeAttribute('disabled');
+      urlInput.removeAttribute('disabled');
+      break;
+    case 'finished':
+      btn.removeAttribute('disabled');
+      urlInput.removeAttribute('disabled');
+      form.reset();
+      urlInput.focus();
+      break;
+    default:
+      break;
+  }
+};
+
+const watch = (elements, state, i18n) => {
+  const watchedState = onChange(state, (path, value) => {
     switch (path) {
-      case 'errors':
+      case 'errKey':
         console.log('errors!');
-        renderErrors(elements, state);
+        renderErrors(elements, state, i18n);
+        break;
+      case 'feeds':
+        console.log('feeds!');
+        console.log(state.posts);
+        renderFeeds(elements, state, i18n);
+        break;
+      case 'form.processState':
+        console.log('state!');
+        handleProcessState(elements, value);
         break;
       default:
         break;
