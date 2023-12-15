a8496af9f55aa6e9418f8b1007f536f078a5dd1e
AnastasiaBrykina <anastasiaaa.6934.@gmail.com>
2023-12-15 13:14:28 +0400

    add src/locales/

---
+++ src/locales/ru.js
@@ -0,0 +1,24 @@
+export default {
+  translation: {
+    errors: {
+      validation: {
+        invalid: 'Ссылка должна быть валидным URL',
+        exists: 'RSS уже существует',
+        valid: 'RSS успешно загружен',
+      },
+      parser: {
+        invalid: 'Ресурс не содержит валидный RSS',
+      },
+      network: {
+        networkErr: 'Произошла ошибка сети',
+      },
+    },
+    feeds: {
+      title: 'Фиды',
+    },
+    posts: {
+      title: 'Посты',
+      button: 'Просмотр',
+    },
+  },
+};
